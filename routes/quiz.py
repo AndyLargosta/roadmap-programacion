c9e25237755d9ddf0b8ef2068bb589f5989d6a12
Sebastian Arguello <sarguello323@gmail.com>
2026-03-16 14:23:07 -0300

    feat: Agregar ruta/funcion para cargar pregunta

--- routes/quiz.py
+++ routes/quiz.py
@@ -1,4 +1,13 @@
-# esto evita que app.py falle al importar los blueprint mientras los archivos estan vacios
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+from services.quiz_service import obtener_pregunta_y_respuestas, verificar_respuesta
 
 quiz = Blueprint("quiz", __name__)
+
+@quiz.route('/pregunta/<int:pregunta_id>', methods=['GET'])
+def cargar_pregunta(pregunta_id):
+    pregunta_data = obtener_pregunta_y_respuestas(pregunta_id)
+
+    if not pregunta_data:
+        return "Pregunta no encontrada", 404
+    
+    return render_template('quiz.html', pregunta=pregunta_data)
