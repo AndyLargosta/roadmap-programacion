3bbcd449c6ccf304243f49a51ce6d0d181b7d861
Samuel Karjallo <karjallo@disroot.org>
2026-03-17 16:29:56 -0300

    feat: agregamos redireccionamiento a login

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, url_for
 from models import db
+import socket
 
 def create_app():
     app = Flask(__name__)
 
-    # configuraciones
     app.config['SECRET_KEY'] = 'hackaton_codepro_2026'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///roadmap.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    # registro de blueprints
     from routes.auth import auth
     from routes.roadmap import roadmap
     from routes.quiz import quiz
@@ -21,6 +21,10 @@ def create_app():
     app.register_blueprint(roadmap)
     app.register_blueprint(quiz)
 
+    @app.route('/')
+    def index():
+        return redirect(url_for('auth.login'))
+
     with app.app_context():
         db.create_all()
 
@@ -28,5 +32,7 @@ def create_app():
 
 
 if __name__ == '__main__':
+    host_name = socket.gethostname()
+    ip = socket.gethostbyname(host_name)  
     app = create_app()
-    app.run(debug=True)
+    app.run(host=ip, debug=True)
